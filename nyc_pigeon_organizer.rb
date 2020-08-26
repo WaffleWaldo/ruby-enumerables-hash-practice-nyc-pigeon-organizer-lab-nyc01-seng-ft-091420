0fd9c3953fd8ce1910ce6a4e4eadb350bfb71777
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-26 01:33:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |key,  value| 
+  data.each_with_object({}) do |(key,  value), new_array|
+    value.each do |inner_key, name|
+      name.each do |
 end
